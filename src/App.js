79efa7873c7b12a7c3620a0ef27dbb46bab3ad47
lamba01 <jamesspaul987@gmail.com>
2024-04-09 19:50:14 +0100

    imported needed assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Typography variant='myvariant'> This is my Typography </Typography>
+    <Typography>I hope you like this </Typography>
+      <Button color='error' variant='contained'>Hello from mui5</Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, createTheme } from '@mui/material';
+// to override theme
+const theme = createTheme({
+  // color palette
+  palette: {
+    primary: {
+      main: "#CCC",
+    },
+  },
+  // typography
+  typography: {
+    fontFamily: [
+      'Overpass',
+      'Ubuntu',
+      'sans-serif',
+    ].join(','),
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+    myvariant: {
+      color: 'hsl(208, 49%, 24%)',
+      fontSize: '1rem'
+    },
+  },
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
